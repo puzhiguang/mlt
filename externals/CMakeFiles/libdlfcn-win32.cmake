024a745c9478a88c945cb125a9a5b28959c37b06
梁泰琪 <liangtaiqi@cvte.com>
2018-05-18 19:58:23 +0800

    compile play demo in windows

--- externals/CMakeFiles/libdlfcn-win32.cmake
+++ externals/CMakeFiles/libdlfcn-win32.cmake
@@ -5,3 +5,6 @@ add_library(libdlfcn STATIC ${SRCS} ${HEADERS})
 set_target_properties(libdlfcn PROPERTIES OUTPUT_NAME "dlfcn-win32")
 
 target_include_directories(libdlfcn PRIVATE ${EXTERNAL_LIBS}/dlfcn-win32)
+
+find_library(psapi Psapi.lib ${WINDOWS_SDK_DIR})
+target_link_libraries(libdlfcn ${psapi})
--- externals/CMakeFiles/libpthread-win32.cmake
+++ externals/CMakeFiles/libpthread-win32.cmake
@@ -1,9 +1,11 @@
 file(GLOB HEADERS ${EXTERNAL_LIBS}/pthread-win32/*.h)
 file(GLOB SRCS ${EXTERNAL_LIBS}/pthread-win32/*.c)
 
-add_library(libpthreadwin32 STATIC ${SRCS} ${HEADERS})
+list(REMOVE_ITEM SRCS ${EXTERNAL_LIBS}/pthread-win32/pthread.c)
+
+add_library(libpthreadwin32 SHARED ${SRCS} ${HEADERS})
 set_target_properties(libpthreadwin32 PROPERTIES OUTPUT_NAME "pthread-win32")
 
-add_definitions(-DHAVE_CONFIG_H -DPTW32_STATIC_LIB)
+add_definitions(-DHAVE_CONFIG_H -DPTW32_BUILD)
 
 target_include_directories(libpthreadwin32 PRIVATE ${EXTERNAL_LIBS}/pthread-win32)
