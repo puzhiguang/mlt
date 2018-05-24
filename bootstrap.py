bc2429e81a027b6b232c3e0ec56d3ae22ea4db27
梁泰琪 <liangtaiqi@cvte.com>
2018-05-24 19:07:50 +0800

    config bootstrap script

--- bootstrap.py
+++ bootstrap.py
@@ -1,15 +1,42 @@
 import os
+import platform
+
+plat=platform.platform()
+sys=platform.system()
 
 root=os.path.dirname(os.path.realpath(__file__))
-sys='osx'
+
 prefix=f'{root}/build/{sys}'
 
-print('============ fetching external libraries ==============')
+print('fetching external libraries')
 os.system('git submodule update --init --recursive')
 
-#build external libs
-os.system('python externals/build_all.py')
+os.chdir(root + '/externals/ffmpeg')
 
-#build mlt
-os.system(f'export PKG_CONFIG_PATH={root}/externals/build/{sys}/lib/pkgconfig && \
-            ./configure --prefix={prefix} && make && make install')
+print("configure ffmpeg, may take a while")
+if(sys=='Windows'):
+    os.system('sh ./configure --enable-shared --disable-static --disable-programs --disable-asm --disable-postproc --toolchain=msvc')
+else:
+    os.system('sh ./configure --enable-shared --disable-static --disable-programs --disable-asm --disable-postproc')
+os.system('make')
+
+os.chdir(root)
+if not os.path.exists('build'):
+    os.mkdir('build',)
+cmakedir=root
+cmakegenerator=''
+if(sys=='Windows'):
+    dest='build/win32'
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+    cmakedir+='/'+dest
+    cmakegenerator='"Visual Studio 15 2017"'
+elif(sys=='MacOS'):
+    dest='build/mac'
+    if not os.path.exists(dest): 
+        os.mkdir(dest)
+    cmakedir+='/'+dest
+    cmakegenerator='Xcode'
+
+os.chdir(cmakedir)
+os.system(f'cmake ../.. -G {cmakegenerator}')
